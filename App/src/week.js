c2d655c41a1372a54398d356b63d0f57e6af48b7
Peter Lord <peter@plord.co.uk>
2022-12-28 22:31:41 +0000

    Add GB locale to avoid crashing in the UK

--- App/src/week.js
+++ App/src/week.js
@@ -1,5 +1,6 @@
 export default {
   en_US: ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'],
+  en_GB: ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'],
   ja_JP: ['にち', 'げつ', 'か', 'すい', 'もく', 'きん', 'ど'],
   ko_KR: ['일', '월', '화', '수', '목', '금', '토'],
 };
